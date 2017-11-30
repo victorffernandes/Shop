72cc75be5ba51fc90a81d4b0f4a3f5e8079ac287
magrelinho <victorfernandes.nave@gmail.com>
2017-11-30 01:18:52 -0200

    Added sell function with some issues, solving then in the next commit

--- functions.py
+++ functions.py
@@ -1,12 +1,44 @@
 import os
 
+def sell(data, nome,cod, qunt):
+    if os.path.exists("cadastro.txt"):
+        product = getProdByCod(cod)
+        if product is not None and qunt <= product[3]:
+            arq = open("vendas.txt", "a")
+            args = [data, nome, cod, qunt]
+            arq.write((" ").join(args)+ " \n")
+            arq.close()
+            updateregistry(product[0],product[1],product[2],str(int(product[3]) - int(qunt)))
+            return True
+        else:
+            return False
+
+
+
+def getProdByCod(cod):
+    arq = open("cadastro.txt", 'r+')
+    v = []
+    for line in arq:
+        v.append(line)
+    arq.close()
+
+    find = None
+    for i in range(len(v)):
+        if v[i].find(cod) != -1:
+            find = v[i]
+    if find == None:
+        return None
+    else:
+        return find.split(" ")
+
+
 def registryProd(cod, des, pre, qunt):
 
     if os.path.exists("cadastro.txt"):
         if not(checkIfProdExists(cod)):
             arq = open("cadastro.txt", "a")
             args = [cod, des, pre, qunt]
-            arq.write((" ").join(args)+ "\n")
+            arq.write((" ").join(args)+ " \n")
             arq.close()
             return True
         else:
@@ -14,7 +46,7 @@ def registryProd(cod, des, pre, qunt):
     else:
         arq = open("cadastro.txt", "a")
         args = [cod, des, pre, qunt]
-        arq.write((" ").join(args)+ "\n" )
+        arq.write((" ").join(args) + " \n")
         arq.close()
         return True
 
@@ -22,34 +54,46 @@ def checkIfProdExists(cod):
     arq = open("cadastro.txt")
     v = []
     for line in arq:
-        print(line)
         if line.find(cod) != -1:
             arq.close()
-            return False
+            return True
     arq.close()
-    return True
+    return False
+
+def showRegistry():
+    if os.path.exists("cadastro.txt"):
+        arq = open("cadastro.txt", "r")
+        v = arq.read()
+        v.replace("\n"," ")
+        print("Registry: "+ v)
+        arq.close()
 
 def updateregistry(cod, des, pre, qunt):
-    arq = open("cadastro.txt",'r+')
-    v = []
-    args = [cod, des, pre, qunt]
-    for line in arq:
-        v.append(line)
-    arq.close()
-
-    for i in range(len(v)):
-        if v[i].find(cod) != -1:
-            v[i] = (" ").join(args) + "\n"
-    arq = open("cadastro.txt", 'r+')
-    arq.write(("").join(v)
-              )
-    print(v)
+    if os.path.exists("cadastro.txt"):
+        arq = open("cadastro.txt",'r+')
+        v = []
+        args = [cod, des, pre, qunt]
+        for line in arq:
+            v.append(line)
+        arq.close()
+        find = False
+        for i in range(len(v)):
+            if v[i].find(cod) != -1:
+                find = True
+                v[i] = (" ").join(args)+ " \n"
+        arq = open("cadastro.txt", 'r+')
+        arq.write((" ").join(v))
+        return find
+    else:
+        return False
 
 
 
 #def sellProd(data, nomeCliente, cod, qunt):
-registryProd("123","","","")
-registryProd("321","","","")
-updateregistry("123","cassetinho","5,0","1")
-updateregistry("321","revolver","500,0","1")
-
+#print(updateregistry("123","cassetinho","5,0","1"))
+showRegistry()
+sell("Hoje","Victor", "456", "1")
+showRegistry()
+registryProd("321","revolver","300","1")
+registryProd("456","espingarda","300","1")
+sell("Hoje","Victor", "321", "1")
