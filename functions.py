aa7203db876d37292f83eb1a65f68a0b9dd905f0
magrelinho <victorfernandes.nave@gmail.com>
2017-12-06 00:24:41 -0200

    Foi

--- functions.py
+++ functions.py
@@ -2,8 +2,8 @@ import os
 
 def sell(data, nome,cod, qunt):
     if os.path.exists("cadastro.txt"):
-        product = getProdByCod(cod)
-        if product is not None and qunt <= product[3]:
+        product = getProdByCod(cod).split()
+        if product is not None and int(qunt) <= int(product[3]):
             arq = open("vendas.txt", "a")
             args = [data, nome, cod, qunt]
             arq.write((" ").join(args)+ " \n")
@@ -29,7 +29,7 @@ def getProdByCod(cod):
     if find == None:
         return None
     else:
-        return find.split(" ")
+        return find#.split(" ")
 
 
 def registryProd(cod, des, pre, qunt):
@@ -67,6 +67,101 @@ def showRegistry(path):
         v.replace("\n"," ")
         arq.close()
         return v
+
+def soldProducts(method):
+    if os.path.exists("vendas.txt") and os.path.exists("cadastro.txt"):
+        arq = open("cadastro.txt","r")
+        products = []
+        for line in arq:
+            products.append(line.split()[0])
+        arq.close()
+
+        arq = open("vendas.txt", "r")
+        quantities = []
+        for cod in products:
+            count = 0
+            arq = open("vendas.txt", "r")
+            for sell in arq:
+                s = sell.split()
+                if cod == s[2]:
+                    count += int(s[3])
+            quantities.append(count)
+            arq.close()
+        chosen = method(quantities)
+        res = []
+        for i in chosen:
+            res.append(getProdByCod(products[i]))
+        return res
+
+def findHigher(v):
+    h = -1
+    l = []
+    for i in range(len(v)):
+        if v[i] > h:
+            h = v[i]
+            l = [i]
+        elif i == h:
+            l.append(i)
+    return l
+
+def findSmaller(v):
+    h = 2**1234567
+    l = []
+    for i in range(len(v)):
+        if v[i] < h:
+            h = v[i]
+            l = [i]
+        elif i == h:
+            l.append(i)
+    return l
+
+def showSellsByDay(string):
+    if os.path.exists("vendas.txt"):
+        arq = open("vendas.txt","r")
+        v = []
+        for line in arq:
+            t = line.split();
+            if string in t[0]:
+                v.append(line)
+        arq.close()
+        if len(v) > 0:
+            return v
+        else:
+            return False
+    return False
+
+def findAllClients():
+    arq = open("vendas.txt","r")
+    c = []
+    for line in arq:
+        cred = line.split()[1]
+        if not(cred in c):
+            c.append(cred)
+    arq.close()
+    return c
+
+def mostValuableClient():
+    if os.path.exists("vendas.txt") and os.path.exists("cadastro.txt"):
+        allClients = findAllClients()
+
+        res = []
+        for c in allClients:
+            c_n = 0.0
+            arq = open("vendas.txt", "r")
+            for line in arq:
+                l = line.split()
+                if c == l[1]:
+                    p = getProdByCod(l[2]).split()
+                    c_n += float(p[2]) * float(l[3])
+            res.append(c_n)
+            arq.close()
+        indexes = findHigher(res)
+        result = []
+        for i in indexes:
+            result.append(allClients[i])
+        return result
+    return False
+
 
 def updateregistry(cod, des, pre, qunt):
     if os.path.exists("cadastro.txt"):
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import functions as f
 CADASTRO = "cadastro.txt"
 VENDAS = "vendas.txt"
 
-f.registryProd("123","Bolsa","2.0","5")
-f.registryProd("312","Bolsa","2.0","5")
-f.updateregistry("123","Davidson","2.0","9")
+f.registryProd("123","lapis","2.0","5")
+f.registryProd("312","Bolsa","4.0","5")
+f.updateregistry("312","Bolsa","4.0","5")
+print(f.sell("12/03","Victor","312","3"))
+#print(f.sell("12/03","Victor","312","4"))
+#print(f.sell("12/02","Victor","123","1"))
+print((f.showRegistry(VENDAS)))
 print((f.showRegistry(CADASTRO)))
-
+print(f.mostValuableClient())
